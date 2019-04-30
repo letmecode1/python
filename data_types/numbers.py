ee003ac492d50546174e3e6f5a9f9571fb5a84c7
root24 <28819306+letmecode1@users.noreply.github.com>
2019-04-29 22:29:21 -0500

    Working with numbers

---
+++ data_types/numbers.py
@@ -0,0 +1,17 @@
+########## Numbers ##########
+
+print(2+2) # addition
+print(10-2) # subtraction
+print(9*9) # multiplication
+print(5/3) # division
+print(5//3) # floor division, always returns a number without a remainder
+print(8%3) # Modulo division, returns the remainder
+print(2 ** 3) # exponent
+
+
+########## Converting Strings to Numbers ##########
+print(type(str(1.1)))
+print(type(int("10")))
+print(type(int(5.9999)))
+print(type(float("5.6")))
+print(type(float(5)))
